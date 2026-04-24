d5fdf9cdb6604646bf3a5c730409527a7cc87268
Sarvesh-N <sarveshnanthakumar99@gmail.com>
2026-04-24 13:38:05 +0530

    second-version

--- terraform_practice_files/first.tf
+++ terraform_practice_files/first.tf
@@ -1,12 +1,25 @@
-provider "aws"{
+provider "aws" {
     region = "ap-south-1"
+    alias = "india"
 }
 
-resource "aws_instance" "terraform-demo" {
-    ami = "ami-05d2d839d4f73aafb"
-    instance_type = "t3.micro"
-    tags = {
-        Name = "demo-instance"
-    }
-    key_name = "ubuntu"
+provider "aws"{
+    region = "us-east-1"
+    alias = "us"
 }
+
+resource "aws_instance" "demo_in_india"{
+    ami = "ami-0e12ffc2dd465f6e4"
+    instance_type = "t3.micro"
+    key_name = "ubuntu"
+    provider = aws.india
+}
+
+resource "aws_instance" "demo_in_us" {
+    ami = "ami-098e39bafa7e7303d"
+    instance_type = "t3.micro"
+    key_name = "ubuntu"
+    provider = aws.us
+}
+
+
