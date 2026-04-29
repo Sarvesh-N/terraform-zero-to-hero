e9186f47e4c4cfa0ba9042fbfd56c1db65266f76
Sarvesh-N <sarveshnanthakumar99@gmail.com>
2026-04-29 20:49:18 +0530

    .

--- terraform_practice_files/main.tf
+++ terraform_practice_files/main.tf
@@ -9,7 +9,7 @@ provider "aws"{
 }
 
 
-resource "aws_security_group" "sg" {
+resource "aws_security_group" "India_sg" {
     ingress {
         description = "ssh access"
         from_port = 22
@@ -33,7 +33,7 @@ resource "aws_security_group" "sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
     tags = {
-      Name : "Demo-sg"
+      Name : "Demo-sg-In"
     }
 }
 
@@ -44,9 +44,37 @@ resource "aws_instance" "In_instance" {
       Name = "demo_ec2_In"
     }
     provider = aws.India
-    vpc_security_group_ids = [aws_security_group.sg.id]
+    vpc_security_group_ids = [aws_security_group.India_sg.id]
 }
 
+
+resource "aws_security_group" "us_sg" {
+    ingress {
+        description = "ssh access"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "http access"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+      Name : "Demo-sg-us"
+    }
+}
 
 resource "aws_instance" "us_instance" {
     ami = var.us_ami
@@ -54,6 +82,7 @@ resource "aws_instance" "us_instance" {
     tags = {
         Name = "demo_ec2_us"
     }
-    vpc_security_group_ids = [aws_security_group.sg.id]
+    provider = aws.us
+    vpc_security_group_ids = [aws_security_group.us_sg.id]
 }
 
