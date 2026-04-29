0acf4d5d5d07a1f4e6121ceed1239743d64d253e
Sarvesh-N <sarveshnanthakumar99@gmail.com>
2026-04-29 20:34:11 +0530

    .

--- terraform_practice_files/main.tf
+++ terraform_practice_files/main.tf
@@ -15,5 +15,34 @@ resource "aws_instance" "In_instance" {
       Name = "demo_ec2"
     }
     provider = aws.India
-    security_groups = [aws_security_group.sg.id]
+    vpc_security_group_ids = [aws_security_group.sg.id]
+}
+
+
+resource "aws_security_group" "sg" {
+    ingress {
+        description = "ssh access"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "http access"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+      Name : "Demo-sg"
+    }
 }
--- terraform_practice_files/variables.tf
+++ terraform_practice_files/variables.tf
@@ -13,27 +13,3 @@ variable "type" {
     type = string
 }
 
-resource "aws_security_group" "sg" {
-    ingress {
-        description = "ssh access"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        description = "http access"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
